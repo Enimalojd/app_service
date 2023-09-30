0854cbe7872767f68f8d89999d6fbedfcefbfac9
Enimalojd <enderston8@gmail.com>
2023-09-30 18:06:32 +0400

    1st api

--- service/services/admin.py
+++ service/services/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from services.models import Service, Subscription, Plan
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'full_price']
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ['plan_type', 'discount_percent']
+
+
+@admin.register(Subscription)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ['client', 'service', 'plan']
